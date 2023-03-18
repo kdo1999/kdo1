aacf2fa8754eb718ec14f68f84a3fe378f97a9b9
user1 <kdo_1999@naver.com>
2023-03-18 17:48:27 +0900

    .

---
+++ src/main/java/com/kdo/kdo/KdoApplication.java
@@ -0,0 +1,13 @@
+package com.kdo.kdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdo/kdo/KdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdo.kdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
